67a5bc40177b020bc87d63a806890baec464fd97
yangbisheng <2606424906@qq.com>
2022-01-31 06:44:04 +0800

    创建构造器 (#259)
    创建构造器 (#259)

--- src/main/java/com/github/hcsp/objectbasic/Cat.java
+++ src/main/java/com/github/hcsp/objectbasic/Cat.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.objectbasic;
 
-public class Cat {
+public class
+Cat {
     /** 猫咪的名字 */
     String name;
     /** 猫咪的年龄 */
@@ -11,11 +12,13 @@ public class Cat {
     /** 创建一只指定名字的猫，age的默认值是1，cute的默认值是true */
     public Cat(String name) {
         // 请在这里使用this()调用其他的构造器
+        this(name, 1, true);
     }
 
     /** 创建一只指定名字和年龄的猫，cute的默认值是true */
     public Cat(String name, int age) {
         // 请在这里使用this()调用其他的构造器
+        this(name, age, true);
     }
 
     /** 创建一只指定名字、年龄和萌属性的猫 */
